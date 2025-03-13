953676708dec03cf24b905f4b00195219be4ecf6
nschlues <nschlues@uccs.edu>
2025-03-13 16:19:34 -0600

    Tried to add an error screen after invalid search. Didn't work, but got more experience in django ;)

--- django_project/amazon/views.py
+++ django_project/amazon/views.py
@@ -2,25 +2,29 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
-# Create your views here.
+#def start_main(request):
+    # Load the first template
+    #template = loader.get_template('main.html')
+    #return HttpResponse(template.render())
+
 def produce_query(request):
 
     # Get the user's raw search
     initial_search = request.GET.get("query", "")
 
     # Check to see if a search was entered
-    if initial_search:
+    if not initial_search:
+      
+        # If search was not entered, go to error page and ask again
+        #return render(request, "no_search.html", {"error" : "Please enter a new search"})
+        return render(request, "main.html", {"error" : "Please enter a new search"})
 
-        # Format the search
-        formatted_search = initial_search.replace(' ', '+')
+    # Format the search
+    formatted_search = initial_search.replace(' ', '+')
 
-        # Format the URL
-        formatted_url = f"https://www.amazon.com/s?k={formatted_search}"
+    # Format the URL
+    formatted_url = f"https://www.amazon.com/s?k={formatted_search}"
 
-        # Redirect user to Amazon page with their input
-        return redirect(formatted_url)
-        
+    # Redirect user to Amazon page with their input
+    return redirect(formatted_url)
 
-
-    
-    return render(request, "no_search.html")
