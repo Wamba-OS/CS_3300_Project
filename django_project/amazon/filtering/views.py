c555f3018e59bc818679e813bb046a9fd54bff09
Skarlin78 <123046977+Skarlin78@users.noreply.github.com>
2025-03-26 00:10:35 -0600

    Task 3 finished

---
+++ django_project/amazon/filtering/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Product
+from .forms import ProductFilterForm
+
+def product_list(request):
+    products = Product.objects.all()
+    form = ProductFilterForm(request.GET)
+
+    if form.is_valid():
+        if form.cleaned_data.get('min_price'):
+            products = products.filter(price__gte=form.cleaned_data['min_price'])
+        if form.cleaned_data.get('max_price'):
+            products = products.filter(price__lte=form.cleaned_data['max_price'])
+        if form.cleaned_data.get('brand'):
+            products = products.filter(brand__icontains=form.cleaned_data['brand'])
+        if form.cleaned_data.get('min_rating'):
+            products = products.filter(rating__gte=form.cleaned_data['min_rating'])
+        if form.cleaned_data.get('category'):
+            products = products.filter(category__icontains=form.cleaned_data['category'])
+
+    return render(request, 'products/product_list.html', {'products': products, 'form': form})
